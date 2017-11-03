a5a5e523115b21ac11da371cd46b9d9b0deba2a7
Guangshan <159384@sh.lianjia.com>
2017-11-03 13:37:53 +0800

    after css hmr

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@
 
 import _ from 'lodash';
 import printMe from './print.js';
+import './style.css';
+
 function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
@@ -31,6 +33,8 @@ document.body.appendChild(component());
 if (module.hot) {
   module.hot.accept('./print.js', function() {
     console.log('Accepting the updated printMe module!');
+    // 因为函数是传递引用，如果这里热替换printMe，其实对于上面的onclick替换是无效的，点击还是触发原来的click.
+    // 参考https://doc.webpack-china.org/guides/hot-module-replacement/
     printMe();
   })
 }
