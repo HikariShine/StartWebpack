0041fe46e0f0ab4fbea843e4640ea0bebd99ac15
Guangshan <159384@sh.lianjia.com>
2017-11-03 16:53:25 +0800

    完成代码分块

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,19 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
+    another: './src/another-module.js'
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Production'
+      title: 'Code Splitting'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common' // 指定公共 bundle 的名称。
     })
   ],
   output: {
